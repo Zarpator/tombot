421ab6cf03a7f76fd1487648851d49c068034d9c
Zarpator <strube.jonas@gmail.com>
2019-03-19 01:10:00 +0100

    add JPA annotations in DbChat class

--- src/main/java/com/zarpator/tombot/datalayer/DbChat.java
+++ src/main/java/com/zarpator/tombot/datalayer/DbChat.java
@@ -1,6 +1,15 @@
 package com.zarpator.tombot.datalayer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class DbChat {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String currentOngoingDialog;
 	private int currentStateInOngoingDialog = 0;
